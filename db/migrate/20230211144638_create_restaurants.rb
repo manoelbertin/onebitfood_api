5b02c19589463bea0b9d22c71a3a3436a2258035
manoel bertin <manoeltirj@gmail.com>
2023-02-11 11:59:48 -0300

    I created all my models, genereted all migrations too.

---
+++ db/migrate/20230211144638_create_restaurants.rb
@@ -0,0 +1,18 @@
+class CreateRestaurants < ActiveRecord::Migration[7.0]
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.text :description
+      t.float :delivery_tax
+      t.string :state
+      t.string :city
+      t.string :street
+      t.string :neighborhood
+      t.string :number
+      t.string :complement
+      t.references :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
