61237cb531f3c8fd8f9ffa160b6dd4acab712874
manoel bertin <manoeltirj@gmail.com>
2023-02-12 08:47:22 -0300

    enum é uma forma de mapear um campo do tipo inteiro, um valor mais fácil de trabalhar dentro do rails. exemplo:          aguardando ou entregue o pedido. É uma forma sem precisar que a gente use String.

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,6 @@ class Order < ApplicationRecord
   has_many :order_products
 
   validates :name, :phone_number, :total_value, :city, :neighborhood, :street, :number, presence: true
+
+  enum status: { waiting: 0, delivered: 1  }
 end
