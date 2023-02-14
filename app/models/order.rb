536875e7dc1bf61ce4aa8ae81e4d87c8b1c0f68c
manoel bertin <manoeltirj@gmail.com>
2023-02-14 17:23:47 -0300

    antes de fazer as validations, criei a função privada que percorre o array de pedidos e calcula o preço final de cada pedido . Falta somar a taxa de delivery ainda.

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,14 @@ class Order < ApplicationRecord
   validates :name, :phone_number, :total_value, :city, :neighborhood, :street, :number, presence: true
 
   enum status: { waiting: 0, delivered: 1  }
+
+  before_validation :set_price
+
+  private
+
+  def set_price
+    final_price = 0
+    order_products.each do |op|
+      final_price += op.quantity * op.product.price 
+  end
 end
