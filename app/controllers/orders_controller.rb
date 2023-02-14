861f249e996d805acede3964fb43b6ad39a3fa1a
manoel bertin <manoeltirj@gmail.com>
2023-02-14 17:37:36 -0300

    aqui no controler order estou criando um novo pedido e mostrando para o usuária da API o status do pedido.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,13 @@
 class OrdersController < ApplicationController
   def create
+    order = Order.new(order_params)
+    if order.save
+      render json: @order, status: :created
+    else
+      render json: order.errors, status: :unprocessable_entity
   end
 
   def show
+    
   end
 end
