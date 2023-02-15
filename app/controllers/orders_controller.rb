9e6dab64dfe8be831eb0a1b8d6409fe82edd845a
manoel bertin <manoeltirj@gmail.com>
2023-02-14 22:23:11 -0300

    criei o json para mostrar SHOW os  meus pedidos e esse jbuilder chama a partial _order. Problema que faltava um campo street na minha tabela orders.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,11 +9,19 @@ class OrdersController < ApplicationController
       render json: order.errors, status: :unprocessable_entity
   end
 
-  def show end;
+  def show 
+  end
 
   private
 
   def set_order
     @order = Order.find(params[:id])
+  end
+
+  def order_params
+    params.require(:order).permit(
+      :name, :phone_number, :city, :neighborhood, :street, :number, :complement,
+      order_products_attributes: [:quantity, :product_id]
+    )
   end
 end
