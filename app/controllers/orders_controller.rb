268f51a264f002d352789ed599f72a11b4c33bce
manoel bertin <manoeltirj@gmail.com>
2023-02-15 07:50:39 -0300

    ajeitei as variáveis na action create para @order e criei na view o retorno create.json.jbuilder. Faltando testar no POstman.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,7 +4,7 @@ class OrdersController < ApplicationController
   def create
     order = Order.new(order_params)
     if order.save
-      render json: @order, status: :created
+      render json: order, status: :created
     else
       render json: order.errors, status: :unprocessable_entity
     end
@@ -21,7 +21,7 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order).permit(
-      :name, :phone_number, :city, :neighborhood, :street, :number, :complement,
+      :name, :phone_number, :restaurant_id, :city, :neighborhood, :street, :number, :complement,
       order_products_attributes: [:quantity, :product_id]
     )
   end
